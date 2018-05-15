adc6337003704653bdf45415cbc39ce7ad970631
rajpanchal <hematkumar.2017@vitstudent.ac.in>
2018-05-15 19:45:42 +0530

    Add DELETE todos/:id route

--- server/server-new.js
+++ server/server-new.js
@@ -47,6 +47,21 @@ app.use(bodyParser.json())
           })
         });
 
+        app.delete("/todos/:id", (req, res) => {
+          var id = req.params.id;
+          if(!ObjectID.isValid(id)){
+            return res.status(404).send();
+          }
+          Todo.findByIdAndRemove(id).then((todo) => {
+            if(!todo){
+              return res.status(404).send();
+            }
+            res.send(todo);
+          }).catch((e) => {
+            res.status(400).send();
+          })
+        });
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
